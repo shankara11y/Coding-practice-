abbb92698fcecd6c65923dc2c514b997875066f8
Shankar Mupanna <smupanna@gmail.com>
2025-12-09 23:40:27 +0530

    added new practice file

---
+++ conditions/conditions.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Excellent ! Grade: A")
+elif marks >= 75:
+    print("Good Grade B")
+elif marks >= 50:
+    print("You passed. Grade C")
+else:
+    print("Sorry, you failed, Grade : F ")
