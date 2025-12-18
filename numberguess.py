44a3d283106347d7131b9bacd95de20a31a8db7a
Shankar Mupanna <smupanna@gmail.com>
2025-12-18 23:32:18 +0530

    added more coding pratice files

---
+++ numberguess.py
@@ -0,0 +1,11 @@
+# Simple number guessing game
+
+import random
+
+number = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == number:
+    print("Correct guess!")
+else:
+    print("Wrong! Number was:", number)
